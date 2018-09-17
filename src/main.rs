8797b833970be94553d5c0c4f508365dff5ef709
Kevin Choubacha <chewbacha@gmail.com>
2018-09-17 08:17:31 -0700

    Several changes
    * Switched to u64 for count
    * Switched to u8 for rng

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,40 @@
 extern crate rand;
 
-use rand::{thread_rng, Rng};
+use rand::prelude::*;
 use std::time::Instant;
+
+#[inline(always)]
+fn shuffle(deck: &mut [i32], rng: &mut impl Rng) {
+    for i in 0..(deck.len() - 2) {
+        let j = rng.gen_range(i as u8, deck.len() as u8) as usize;
+        let swap = deck[i];
+        deck[i] = deck[j];
+        deck[j] = swap;
+    }
+}
+
+#[inline(always)]
+fn compare(deck: &[i32], other: &[i32]) -> bool {
+    if deck.len() != other.len() { return false }
+
+    for i in 0..deck.len() {
+        if deck[i] != other[i] { return false }
+    }
+    true
+}
 
 fn main() {
     let deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
     let mut shuffled_deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
-    let mut rng = thread_rng();
+    let mut rng = SmallRng::from_entropy();
     let start = Instant::now();
-    let mut shuffles = 0u128;
+    let mut shuffles = 0u64;
 
     loop {
-        rng.shuffle(&mut shuffled_deck);
+        shuffle(&mut shuffled_deck, &mut rng);
         shuffles += 1;
-        if shuffled_deck == deck {
+        if compare(&deck, &shuffled_deck) {
             println!("We did it! It only took {} shuffles", shuffles);
             break;
         } else if shuffles % 10_000_000 == 0 {
@@ -23,7 +43,7 @@ fn main() {
                 "{} shuffles later... at ~{} shuffles/sec",
                 shuffles,
                 1_000 * shuffles
-                    / (elapsed.as_secs() * 1_000 + elapsed.subsec_millis() as u64) as u128,
+                    / (elapsed.as_secs() * 1_000 + elapsed.subsec_millis() as u64),
             );
         }
     }
