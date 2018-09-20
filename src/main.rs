0c79d0b9e542e3d33d946ec43bd224bebd8e24d4
Kevin Choubacha <chewbacha@gmail.com>
2018-09-20 11:15:28 -0700

    Xor shift

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use rand::prelude::*;
+use rand::prng::XorShiftRng;
 use std::time::Instant;
 
 #[inline(always)]
@@ -15,10 +16,14 @@ fn shuffle(deck: &mut [i32], rng: &mut impl Rng) {
 
 #[inline(always)]
 fn compare(deck: &[i32], other: &[i32]) -> bool {
-    if deck.len() != other.len() { return false }
+    if deck.len() != other.len() {
+        return false;
+    }
 
     for i in 0..deck.len() {
-        if deck[i] != other[i] { return false }
+        if deck[i] != other[i] {
+            return false;
+        }
     }
     true
 }
@@ -27,7 +32,7 @@ fn main() {
     let deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
     let mut shuffled_deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
-    let mut rng = SmallRng::from_entropy();
+    let mut rng = XorShiftRng::from_entropy();
     let start = Instant::now();
     let mut shuffles = 0u64;
 
@@ -42,8 +47,7 @@ fn main() {
             println!(
                 "{} shuffles later... at ~{} shuffles/sec",
                 shuffles,
-                1_000 * shuffles
-                    / (elapsed.as_secs() * 1_000 + elapsed.subsec_millis() as u64),
+                1_000 * shuffles / (elapsed.as_secs() * 1_000 + elapsed.subsec_millis() as u64),
             );
         }
     }
